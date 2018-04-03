c5312d3a57bfb2d8561004b91ffec0d872ba081f
Vincent <vpmalley@gmail.com>
2018-04-03 23:47:33 +0200

    retrieving all pictures for an album

--- app/src/main/java/fr/vpm/mypix/AlbumFragment.java
+++ app/src/main/java/fr/vpm/mypix/AlbumFragment.java
@@ -4,29 +4,28 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import fr.vpm.mypix.album.Album;
+import fr.vpm.mypix.flickr.FlickrAlbumRetriever;
+import fr.vpm.mypix.flickr.services.FlickrRetrofit;
+import fr.vpm.mypix.local.LocalAlbumRetriever;
+import retrofit2.Retrofit;
 
-/**
- * A fragment representing a single Item detail screen.
- * This fragment is either contained in a {@link AlbumListActivity}
- * in two-pane mode (on tablets) or a {@link AlbumActivity}
- * on handsets.
- */
 public class AlbumFragment extends Fragment {
-  /**
-   * The fragment argument representing the item ID that this fragment
-   * represents.
-   */
+
   public static final String ARG_ALBUM_ID = "album_id";
   public static final String ARG_ALBUM_SOURCE = "album_source";
+  private LocalAlbumRetriever localAlbumRetriever;
+  private FlickrAlbumRetriever flickrAlbumRetriever;
 
   private String albumId;
   private Album.Source albumSource;
+
+  private RecyclerView picturesRecyclerView;
 
   /**
    * Mandatory empty constructor for the fragment manager to instantiate the
@@ -47,9 +46,19 @@ public class AlbumFragment extends Fragment {
       albumSource = (Album.Source) getArguments().getSerializable(ARG_ALBUM_SOURCE);
 
       Activity activity = this.getActivity();
-      CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+      CollapsingToolbarLayout appBarLayout = activity.findViewById(R.id.toolbar_layout);
       if (appBarLayout != null) {
         appBarLayout.setTitle(albumSource.name());
+      }
+      localAlbumRetriever = new LocalAlbumRetriever();
+      Retrofit flickrRetrofit = new FlickrRetrofit().getFlickrRetrofit(getContext());
+      flickrAlbumRetriever = new FlickrAlbumRetriever(flickrRetrofit);
+
+      if (Album.Source.FLICKR == albumSource) {
+        flickrAlbumRetriever.getFlickrAlbum(this, albumId);
+      }
+      if (Album.Source.LOCAL == albumSource) {
+        localAlbumRetriever.getLocalAlbum(getContext(), this, albumId);
       }
     }
   }
@@ -58,10 +67,12 @@ public class AlbumFragment extends Fragment {
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
     View rootView = inflater.inflate(R.layout.item_detail, container, false);
-
-    TextView mainLabel = rootView.findViewById(R.id.item_detail);
-    mainLabel.setText(albumId);
-
+    picturesRecyclerView = rootView.findViewById(R.id.item_detail);
     return rootView;
   }
+
+  public void onAlbumRetrieved(final Album album) {
+    // display the album in the recycler view
+  }
+
 }
