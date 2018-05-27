73bc479566b786881c9b3cea0152aad8383c4069
Vincent <vpmalley@gmail.com>
2018-05-27 21:38:05 +0200

    new screen graphical components

--- app/src/main/java/fr/vpm/mypix/PicturesComparisonActivity.java
+++ app/src/main/java/fr/vpm/mypix/PicturesComparisonActivity.java
@@ -7,26 +7,15 @@ import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class PicturesComparisonActivity extends AppCompatActivity {
-  /**
-   * Whether or not the system UI should be auto-hidden after
-   * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-   */
-  private static final boolean AUTO_HIDE = true;
-
-  /**
-   * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-   * user interaction before hiding the system UI.
-   */
-  private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
   /**
    * Some older devices needs a small delay between UI widget updates
    * and a change of the status and navigation bar.
@@ -38,11 +27,6 @@ public class PicturesComparisonActivity extends AppCompatActivity {
     @SuppressLint("InlinedApi")
     @Override
     public void run() {
-      // Delayed removal of status and navigation bar
-
-      // Note that some of these constants are new as of API 16 (Jelly Bean)
-      // and API 19 (KitKat). It is safe to use them, as they are inlined
-      // at compile-time and do nothing on earlier devices.
       mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
           | View.SYSTEM_UI_FLAG_FULLSCREEN
           | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -51,16 +35,17 @@ public class PicturesComparisonActivity extends AppCompatActivity {
           | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
   };
-  private View mControlsView;
+  private ImageView imageViewPicture1;
+  private ImageView imageViewPicture2;
+  private SeekBar visibilitySeekbar;
   private final Runnable mShowPart2Runnable = new Runnable() {
     @Override
     public void run() {
-      // Delayed display of UI elements
       ActionBar actionBar = getSupportActionBar();
       if (actionBar != null) {
         actionBar.show();
       }
-      mControlsView.setVisibility(View.VISIBLE);
+      visibilitySeekbar.setVisibility(View.VISIBLE);
     }
   };
   private boolean mVisible;
@@ -70,25 +55,10 @@ public class PicturesComparisonActivity extends AppCompatActivity {
       hide();
     }
   };
-  /**
-   * Touch listener to use for in-layout UI controls to delay hiding the
-   * system UI. This is to prevent the jarring behavior of controls going away
-   * while interacting with activity UI.
-   */
-  private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-      if (AUTO_HIDE) {
-        delayedHide(AUTO_HIDE_DELAY_MILLIS);
-      }
-      return false;
-    }
-  };
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
     setContentView(R.layout.activity_pictures_comparison);
     ActionBar actionBar = getSupportActionBar();
     if (actionBar != null) {
@@ -96,31 +66,22 @@ public class PicturesComparisonActivity extends AppCompatActivity {
     }
 
     mVisible = true;
-    mControlsView = findViewById(R.id.fullscreen_content_controls);
-    mContentView = findViewById(R.id.fullscreen_content);
+    mContentView = findViewById(R.id.contentView);
+    imageViewPicture1 = findViewById(R.id.picture1);
+    imageViewPicture2 = findViewById(R.id.picture2);
+    visibilitySeekbar = findViewById(R.id.pictures_visibility_seekbar);
 
-
-    // Set up the user interaction to manually show or hide the system UI.
     mContentView.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        toggle();
+        toggleVisibility();
       }
     });
-
-    // Upon interacting with UI controls, delay any scheduled hide()
-    // operations to prevent the jarring behavior of controls going away
-    // while interacting with the UI.
-    findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
   }
 
   @Override
   protected void onPostCreate(Bundle savedInstanceState) {
     super.onPostCreate(savedInstanceState);
-
-    // Trigger the initial hide() shortly after the activity has been
-    // created, to briefly hint to the user that UI controls
-    // are available.
     delayedHide(100);
   }
 
@@ -128,14 +89,13 @@ public class PicturesComparisonActivity extends AppCompatActivity {
   public boolean onOptionsItemSelected(MenuItem item) {
     int id = item.getItemId();
     if (id == android.R.id.home) {
-      // This ID represents the Home or Up button.
       NavUtils.navigateUpFromSameTask(this);
       return true;
     }
     return super.onOptionsItemSelected(item);
   }
 
-  private void toggle() {
+  private void toggleVisibility() {
     if (mVisible) {
       hide();
     } else {
@@ -149,22 +109,19 @@ public class PicturesComparisonActivity extends AppCompatActivity {
     if (actionBar != null) {
       actionBar.hide();
     }
-    mControlsView.setVisibility(View.GONE);
+    visibilitySeekbar.setVisibility(View.GONE);
     mVisible = false;
 
-    // Schedule a runnable to remove the status and navigation bar after a delay
     mHideHandler.removeCallbacks(mShowPart2Runnable);
     mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
   }
 
   @SuppressLint("InlinedApi")
   private void show() {
-    // Show the system bar
     mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
     mVisible = true;
 
-    // Schedule a runnable to display UI elements after a delay
     mHideHandler.removeCallbacks(mHidePart2Runnable);
     mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
   }
