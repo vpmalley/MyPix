cc9c1d1a5c25c2b21c06a1175ebc5e77808a66a7
Vincent <vpmalley@gmail.com>
2018-06-02 00:33:10 +0200

    removed the full screen part of the activity

--- app/src/main/java/fr/vpm/mypix/PicturesComparisonActivity.java
+++ app/src/main/java/fr/vpm/mypix/PicturesComparisonActivity.java
@@ -1,16 +1,13 @@
 package fr.vpm.mypix;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 
@@ -31,39 +28,10 @@ public class PicturesComparisonActivity extends AppCompatActivity {
   public static final String ARG_PICTURE2_URL = PicturesComparisonActivity.class.getName() + "picture2_url";
   public static final int MAX_OPACITY_PROGRESS = 255;
   public static final int MIN_OPACITY_PROGRESS = 0;
-  /**
-   * Some older devices needs a small delay between UI widget updates
-   * and a change of the status and navigation bar.
-   */
-  private static final int UI_ANIMATION_DELAY = 300;
-  private final Handler mHideHandler = new Handler();
-  private View mContentView;
-  private final Runnable mHidePart2Runnable = new Runnable() {
-    @SuppressLint("InlinedApi")
-    @Override
-    public void run() {
-      mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-          | View.SYSTEM_UI_FLAG_FULLSCREEN
-          | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-          | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-          | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-          | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-    }
-  };
+
   private ImageView imageViewPicture1;
   private ImageView imageViewPicture2;
   private SeekBar visibilitySeekbar;
-  private final Runnable mShowPart2Runnable = new Runnable() {
-    @Override
-    public void run() {
-      ActionBar actionBar = getSupportActionBar();
-      if (actionBar != null) {
-        actionBar.show();
-      }
-    }
-  };
-  private boolean mVisible;
-  private final Runnable mHideRunnable = () -> hide();
 
   public static void start(Context context, Picture picture1, Picture picture2) {
     Intent intent = new Intent(context, PicturesComparisonActivity.class);
@@ -89,16 +57,12 @@ public class PicturesComparisonActivity extends AppCompatActivity {
       actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
-    mVisible = true;
-    mContentView = findViewById(R.id.contentView);
     imageViewPicture1 = findViewById(R.id.picture1);
     imageViewPicture2 = findViewById(R.id.picture2);
     visibilitySeekbar = findViewById(R.id.pictures_visibility_seekbar);
     fillPicture1(getIntent());
     fillPicture2(getIntent());
     setupSeekbar();
-
-    mContentView.setOnClickListener(view -> toggleVisibility());
   }
 
   private void fillPicture1(Intent intent) {
@@ -152,12 +116,6 @@ public class PicturesComparisonActivity extends AppCompatActivity {
   }
 
   @Override
-  protected void onPostCreate(Bundle savedInstanceState) {
-    super.onPostCreate(savedInstanceState);
-    delayedHide(100);
-  }
-
-  @Override
   public boolean onOptionsItemSelected(MenuItem item) {
     int id = item.getItemId();
     if (id == android.R.id.home) {
@@ -165,44 +123,5 @@ public class PicturesComparisonActivity extends AppCompatActivity {
       return true;
     }
     return super.onOptionsItemSelected(item);
-  }
-
-  private void toggleVisibility() {
-    if (mVisible) {
-      hide();
-    } else {
-      show();
-    }
-  }
-
-  private void hide() {
-    // Hide UI first
-    ActionBar actionBar = getSupportActionBar();
-    if (actionBar != null) {
-      actionBar.hide();
-    }
-    mVisible = false;
-
-    mHideHandler.removeCallbacks(mShowPart2Runnable);
-    mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-  }
-
-  @SuppressLint("InlinedApi")
-  private void show() {
-    mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-    mVisible = true;
-
-    mHideHandler.removeCallbacks(mHidePart2Runnable);
-    mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-  }
-
-  /**
-   * Schedules a call to hide() in delay milliseconds, canceling any
-   * previously scheduled calls.
-   */
-  private void delayedHide(int delayMillis) {
-    mHideHandler.removeCallbacks(mHideRunnable);
-    mHideHandler.postDelayed(mHideRunnable, delayMillis);
   }
 }
