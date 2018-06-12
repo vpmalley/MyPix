d4950ec88b79175e8a0f0e86395203c668b6b48f
Vincent <vpmalley@gmail.com>
2018-06-12 08:55:25 +0200

    sorting pictures

--- app/src/main/java/fr/vpm/mypix/AlbumFragment.java
+++ app/src/main/java/fr/vpm/mypix/AlbumFragment.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import fr.vpm.mypix.album.Album;
@@ -146,6 +147,7 @@ public class AlbumFragment extends Fragment {
     allAlbums.add(album);
     PicturesRecyclerViewAdapter adapter = (PicturesRecyclerViewAdapter) picturesRecyclerView.getAdapter();
     allPictures.addAll(album.getPictures());
+    Collections.sort(allPictures, (pic1, pic2) -> pic1.getFileName().compareTo(pic2.getFileName()));
     adapter.setPictures(allPictures);
     adapter.notifyDataSetChanged();
   }
