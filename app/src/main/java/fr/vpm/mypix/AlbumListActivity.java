b2b82a54952d67cfb9922f114584bdf847368d52
Vincent Malley <vincent.malley@traveldoo.com>
2018-07-12 09:51:26 +0200

    removed fab off the album list

--- app/src/main/java/fr/vpm/mypix/AlbumListActivity.java
+++ app/src/main/java/fr/vpm/mypix/AlbumListActivity.java
@@ -2,8 +2,6 @@ package fr.vpm.mypix;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -41,15 +39,7 @@ public class AlbumListActivity extends AppCompatActivity {
     setSupportActionBar(toolbar);
     toolbar.setTitle(getTitle());
 
-    FloatingActionButton fab = findViewById(R.id.fab);
-    fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        .setAction("Action", null).show());
-
     if (findViewById(R.id.item_detail_container) != null) {
-      // The detail container view will be present only in the
-      // large-screen layouts (res/values-w900dp).
-      // If this view is present, then the
-      // activity should be in two-pane mode.
       mTwoPane = true;
     }
 
